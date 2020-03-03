7cba62946af88acac7cc623085cc4a6b74167fed
davidfannar98 <davidfannar98@gmail.com>
2020-03-03 17:32:52 +0000

    about page

--- template/src/components/App.js
+++ template/src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import About from "./components/About";
 
 const App = () => {
-    return <p>Start working here!</p>
-};
+    return (
+      <div>
+        <Switch>
+          <Route path="/about" component={ About }/>
+  
+          <Route path="/contact/:id">
+            <Contact />
+          </Route>
+          <Route path="/contact">
+            <AllContacts />
+          </Route>
+          <Route path="/">
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
 
 export default App;
