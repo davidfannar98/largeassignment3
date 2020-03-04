56037f6bcc6cb4dcc06f4e176e84fa72a7f2c68c
nonnikb <42572417+nonnikb@users.noreply.github.com>
2020-03-04 12:03:48 +0000

    update

--- template/src/components/NavLinks/NavLinks.js
+++ template/src/components/NavLinks/NavLinks.js
@@ -1,12 +1,8 @@
-/*import React from 'react';
+import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-class NavLinks extends React.Component {
-    render(){
-        return<ul className="nav-links">
-            <nav>
-                <NavLink to="/"
-                         className="navigation-link">*/
+
+
 
 
 
